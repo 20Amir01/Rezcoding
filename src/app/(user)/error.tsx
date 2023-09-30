8d7e1dac6677f96f8781500614546b48426d6262
20Amir01 <96217175+20Amir01@users.noreply.github.com>
2023-09-30 20:30:04 +0330

    feat : error page

--- src/app/(user)/error.tsx
+++ src/app/(user)/error.tsx
@@ -1,30 +1,33 @@
-'use client' // Error components must be Client Components
- 
-import { useEffect } from 'react'
- 
+"use client"; // Error components must be Client Components
+
+import ClientSideRoute from "@/components/ClientSideRoute";
+import PaddingContainer from "@/components/layout/PaddingContainer";
+import { useEffect } from "react";
+
 export default function Error({
   error,
   reset,
 }: {
-  error: Error
-  reset: () => void
+  error: Error;
+  reset: () => void;
 }) {
   useEffect(() => {
     // Log the error to an error reporting service
-    console.error(error)
-  }, [error])
- 
+    console.error(error);
+  }, [error]);
+
   return (
-    <div>
-      <h2>Something went wrong!</h2>
-      <button
-        onClick={
-          // Attempt to recover by trying to re-render the segment
-          () => reset()
-        }
-      >
-        Try again
-      </button>
-    </div>
-  )
+    <PaddingContainer>
+      <div className="flex justify-center items-center h-[100dvh] gap-1 flex-col text-neutral-800 text-[90%] md:text-[110%]">
+        <h2>مشکلی در درخواست شما به به وجود آمده است !</h2>
+        <p>لطفا به صفحه اصلی سایت بازگردید .</p>
+        <ClientSideRoute
+          route="/"
+          className="px-2.5 py-1 bg-neutral-800 text-neutral-100 rounded-md"
+        >
+          صفحه اصلی
+        </ClientSideRoute>
+      </div>
+    </PaddingContainer>
+  );
 }
