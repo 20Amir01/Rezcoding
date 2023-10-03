4973f3dbbced1aa740f7cbc9cbc28a22461ee25b
20Amir01 <96217175+20Amir01@users.noreply.github.com>
2023-10-03 03:53:37 +0330

    change : remove hover from post-card

--- src/components/post/PostCard.tsx
+++ src/components/post/PostCard.tsx
@@ -20,7 +20,7 @@ const PostCard = ({
   return (
     <ClientSideRoute
       route={`/post/${post.slug.current}`}
-      className={`px-2.5 py-10 rounded-md gap-2 bg-white shadow-sm md:hover:shadow-lg ${
+      className={`px-2.5 py-10 rounded-md gap-2 bg-white shadow-sm ${
         layout === "horizontal"
           ? `grid grid-cols-1 sm:gap-5 sm:grid-cols-2 md:gap-5 md:px-5 justify-center items-center sm:col-span-2 md:col-span-3  `
           : "w-full flex flex-col"
