01d1431828c6b0615fb0b4e48d9bea9ebc0a4da6
20Amir01 <96217175+20Amir01@users.noreply.github.com>
2023-10-07 01:36:20 +0330

    edit : few styles in homepage ; navlinks have larger font-size and padding and w-full in mobile now

--- src/app/(user)/page.tsx
+++ src/app/(user)/page.tsx
@@ -37,14 +37,16 @@ const HomePage = async () => {
                   «یادگیری» یاد می‌گیرید. ”
                 </p>
                 <cite className="text-white text-opacity-70">
+                  <Link href="https://en.wikipedia.org/wiki/Nicholas_Negroponte#:~:text=Nicholas%20Negroponte%20(born%20December%201,per%20Child%20Association%20(OLPC)." prefetch={false} scroll={false}>
                   نیکلاس نگروپونته
+                  </Link>
                 </cite>
               </blockquote>
             </div>
           </div>
         </div>
       </BgFixedDiv>
-      <div className="w-full h-auto">
+      <div className="w-full h-auto bg-neutral-50">
         <div className="grid grid-cols-1 w-full h-full gap-5">
           <PaddingContainer>
             <div className="w-full mt-16 flex items-center flex-row justify-center gap-10">
@@ -60,10 +62,13 @@ const HomePage = async () => {
               </div>
             </div>
             <div className="rounded-md w-full h-auto flex flex-col gap-10 text-lg p-5">
-              <p className="mx-auto max-w-md text-justify bg-purple-primary bg-opacity-60 border-radius-custom-1 p-10 rounded-md text-neutral-100">
+              <p className="mx-auto max-w-md text-justify bg-blue-primary bg-opacity-100 border-radius-custom-1 p-10 rounded-md text-neutral-100">
                 در این وبسایت مطالب و مقالاتی از موضوعات مختلف مانند کامپیوتر و
                 برنامه نویسی قرار خواهد گرفت که از بخش{" "}
-                <Link className="animate-pulse font-semibold" href="/blog">
+                <Link
+                  className="animate-pulse"
+                  href="/blog"
+                >
                   وبلاگ
                 </Link>{" "}
                 قابل دسترسی و مطالعه هستند .
@@ -78,17 +83,19 @@ const HomePage = async () => {
             </div>
           </PaddingContainer>
           <div className="rounded-md w-full h-auto flex justify-center items-center text-lg">
-            <PaddingContainer>
-              <p className="text-center">
-                اگر تمایل دارید کمی با من آشنا بشید میتونید به بخش{" "}
-                <Link
-                  className="text-purple-primary animate-pulse font-semibold"
-                  href="/about-me"
-                >
-                  من کیم ؟
-                </Link>{" "}
-                وبسایت مراجعه کنید .
-              </p>
+            <PaddingContainer className="w-full h-full">
+              <div className="w-full flex justify-center items-center p-10 bg-gradient-custom-animation border-radius-custom-2 max-w-2xl mx-auto">
+                <p className="text-justify text-neutral-50">
+                  اگر تمایل دارید کمی با من آشنا بشید میتونید به بخش{" "}
+                  <Link
+                    className="animate-pulse"
+                    href="/about-me"
+                  >
+                    من کیم ؟
+                  </Link>{" "}
+                  وبسایت مراجعه کنید .
+                </p>
+              </div>
             </PaddingContainer>
           </div>
         </div>
